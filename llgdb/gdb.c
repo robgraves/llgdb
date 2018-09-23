5e64e2700300d4f3a9646ca934ed2f4bcb8bdc29
Rob Graves <matthewpage2003@yahoo.com>
2018-09-23 19:41:14 -0400

    Working on gdb.c

--- llgdb/gdb.c
+++ llgdb/gdb.c
@@ -11,7 +11,38 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<game.h>
-#include<list.h>
+//#include<game.h>
+//#include<list.h>
 
+void menu();
 
+int main()
+{
+    menu();
+
+    return 0;
+}
+
+//menu definition
+void menu()
+{
+    int input   =   0;
+
+    while (input != 3)
+    {
+        system("clear");
+        printf("MATT'S BOARD GAME DATABASE\n");
+        printf("Please choose from the following options: \n");
+        printf("1 - Do something\n");
+        printf("2 - Do something else\n");
+        printf("3 - Quit\n");
+
+        scanf("%d", &input);
+
+        printf("You chose %d\n", input);
+    }
+
+    printf("Thank you for using Matt's Board Game Database!\n");
+
+    return 0;
+}
