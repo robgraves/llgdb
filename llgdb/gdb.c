69be95e13c5263ada7f6ee6ba21d6a2ad9f74cb0
Rob Graves <matthewpage2003@yahoo.com>
2018-09-23 13:30:18 -0400

    added llgdb/bgdb.c

---
+++ llgdb/gdb.c
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////
+//
+// Board Game DataBase
+//
+// bgdb.c - a program to organize and randomize
+//          board game for selection by using
+//          certain crieria, build on a linked
+//          list.
+//
+/////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<game.h>
+#include<list.h>
+
+
