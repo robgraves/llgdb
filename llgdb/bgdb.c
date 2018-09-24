6f112d0a68778b0e57d0bf9714d07c0db6f90cca
Rob Graves <matthewpage2003@yahoo.com>
2018-09-24 08:29:18 -0400

    updated bgdb.c and menu.h

--- llgdb/bgdb.c
+++ llgdb/bgdb.c
@@ -24,28 +24,3 @@ int main()
     return 0;
 }
 
-//menu definition
-/*
-void menu()
-{
-    int input   =   0;
-
-    while (input != 3)
-    {
-        system("clear");
-        printf("MATT'S BOARD GAME DATABASE\n");
-        printf("Please choose from the following options: \n");
-        printf("1 - Do something\n");
-        printf("2 - Do something else\n");
-        printf("3 - Quit\n");
-
-        scanf("%d", &input);
-
-        printf("You chose %d\n", input);
-    }
-
-    printf("Thank you for using Matt's Board Game Database!\n");
-
-    return 0;
-}
-*/
--- llgdb/menu.h
+++ llgdb/menu.h
@@ -2,27 +2,39 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-//void menu();
-
 void menu()
 {
     int input   =   0;
+    int junk    =   0;
 
     while (input != 3)
     {
         system("clear");
         printf("MATT'S BOARD GAME DATABASE\n");
         printf("Please choose from the following options: \n");
-        printf("1 - Do something\n");
-        printf("2 - Do something else\n");
+        printf("1 - Randomizer\n");
+        printf("2 - Edit Board Game Database\n");
         printf("3 - Quit\n");
 
         scanf("%d", &input);
 
         printf("You chose %d\n", input);
+
+        switch(input)
+        {
+            case 1:
+            //randomizer();
+            printf("This is inside the randomizer!\n");
+            scanf("%d", &junk);
+            break;
+            case 2:
+            //editmenu();
+            printf("This is inside the edit menu");
+            scanf("%d", &junk);
+            break;
+        }        
     }
 
     printf("Thank you for using Matt's Board Game Database!\n");
 
-    //return 0;
 }
